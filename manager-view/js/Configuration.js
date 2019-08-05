0eaaccb3db5e55c3d206e7af67b2c8db59f22805
jichuangsi <43458863+jichuangsi@users.noreply.github.com>
2019-08-05 18:43:44 +0800

    Update Configuration.js
    js配置路径

--- manager-view/js/Configuration.js
+++ manager-view/js/Configuration.js
@@ -2,7 +2,7 @@ var url;
 var token;
 //修改路径
 function httpUrl() {
-	url = "http://192.168.31.83:8083"
+	url = "https://api.dailymanager.jichuangsi.com"
 	//url='http://192.168.5.12:8083'
 	//url='http://192.168.31.145:8083'
 	return url;
