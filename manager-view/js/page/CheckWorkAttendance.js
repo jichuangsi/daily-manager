fb7dd75fe239f07a4b80474721cb2768a01e3dbb
zhoupengg <45555662+zhoupengg@users.noreply.github.com>
2020-06-13 15:59:43 +0800

    [zhoupeng]

--- manager-view/js/page/CheckWorkAttendance.js
+++ manager-view/js/page/CheckWorkAttendance.js
@@ -10,10 +10,10 @@ layui.use(['form', 'table', 'laydate'], function() {
 	function getUser() {
 		var user = JSON.parse(sessionStorage.getItem('userInfo'))
 		getRoleDep(user);
-		if(user.roleName == "M") {
+		if (user.roleName == "M") {
 			user.wechat = '';
 		}
-		if(user.roleName == '员工') {
+		if (user.roleName == '员工') {
 			$('#name').hide();
 		}
 		return user;
@@ -61,9 +61,9 @@ layui.use(['form', 'table', 'laydate'], function() {
 					title: '考勤',
 					align: 'center',
 					templet: function(d) {
-						if(d.stuas == 1) {
+						if (d.stuas == 1) {
 							return "上班打卡"
-						} else if(d.stuas == 2) {
+						} else if (d.stuas == 2) {
 							return "下班打卡"
 						}
 					}
@@ -72,8 +72,9 @@ layui.use(['form', 'table', 'laydate'], function() {
 					title: '打卡时间',
 					align: 'center',
 					templet: function(d) {
-						if(d.chockinTime != 0) {
-							return new Date(+new Date(d.chockinTime) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
+						if (d.chockinTime != 0) {
+							return new Date(+new Date(d.chockinTime) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(
+								/\.[\d]{3}Z/, '')
 						} else {
 							return "-"
 						}
@@ -84,8 +85,9 @@ layui.use(['form', 'table', 'laydate'], function() {
 					title: '考勤时间',
 					align: 'center',
 					templet: function(d) {
-						if(d.time != 0) {
-							return new Date(+new Date(d.time) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
+						if (d.time != 0) {
+							return new Date(+new Date(d.time) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/,
+								'')
 						} else {
 							return "-"
 						}
@@ -106,7 +108,7 @@ layui.use(['form', 'table', 'laydate'], function() {
 			var arr;
 			var code;
 			var total;
-			if(res.code == "0010") {
+			if (res.code == "0010") {
 				code = 0;
 				arr = res.data;
 				total = res.pageSize;
@@ -142,9 +144,9 @@ layui.use(['form', 'table', 'laydate'], function() {
 	form.on('submit(sreach)', function(data) {
 		var param = data.field;
 		var date = param.date.split('~');
-		if(param.date == '') {
+		if (param.date == '') {
 			var date = new Date();
-//			date.setMonth(date.getMonth() - 1);
+			//			date.setMonth(date.getMonth() - 1);
 			var dateStart = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 			var dateEnd = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 		} else {
@@ -152,20 +154,20 @@ layui.use(['form', 'table', 'laydate'], function() {
 			var dateEnd = date[1];
 			var startTime = new Date(dateStart); // 开始时间
 			var endTime = new Date(dateEnd); // 结束时间
-			if(Math.floor((endTime - startTime) / 1000 / 60 / 60 / 24) > 93) {
+			if (Math.floor((endTime - startTime) / 1000 / 60 / 60 / 24) > 93) {
 				var date = new Date();
 				date.setMonth(date.getMonth() - 1);
-				var dateStart = date.getFullYear() + "-" + (date.getMonth()-1) + "-" + date.getDate();
+				var dateStart = date.getFullYear() + "-" + (date.getMonth() - 1) + "-" + date.getDate();
 				var dateEnd = date.getFullYear() + "-" + (date.getMonth() + 2) + "-" + date.getDate();
 			}
 		}
-		if(param.roleDept==undefined){
-			temp1 = setExcel(' ',dateStart,dateEnd);
-			
-		}else{
+
+		if (param.roleDept == undefined) {
+			temp1 = setExcel(' ', dateStart, dateEnd);
+		} else {
 			temp1 = setExcel(param.roleDept, dateEnd, dateStart);
 		}
-		
+		console.log(temp1);
 		table.reload('idTest', {
 			url: httpUrl() + '/kq/getDailyList',
 			header: {
@@ -193,12 +195,12 @@ layui.use(['form', 'table', 'laydate'], function() {
 				'accessToken': getToken()
 			},
 			success: function(res) {
-				if(res.code == '0010') {
+				if (res.code == '0010') {
 					arr = res.data;
-					if(arr.length == 0) {
+					if (arr.length == 0) {
 						$('#allDept').empty();
 					} else {
-						for(var i = 0; i < arr.length; i++) {
+						for (var i = 0; i < arr.length; i++) {
 							options += '<option value="' + arr[i].deptId + '" >' + arr[i].deptName + '</option>'
 						}
 						$('#roleDept').append(options);
@@ -216,8 +218,47 @@ layui.use(['form', 'table', 'laydate'], function() {
 	temp1 = setExcel(getUser().deptId, dateStart, dateEnd);
 	//获取全部条件查询的考勤数据
 	function setExcel(deptId, dateStart, dateEnd) {
-		var temp;
-		$.ajax({
+
+		var url = "/kq/importDailyList?dpid=" + deptId + "&timeStart=" + dateStart + "&timeEnd=" + dateEnd;
+		var getWeekUrl = "/kq/importStatisticsByWeek?dpid=" + deptId + "&timeStart=" + dateStart + "&timeEnd=" + dateEnd;
+		var getMonthUrl = "/kq/importStatisticsByMonth?dpid=" + deptId + "&timeStart=" + dateStart + "&timeEnd=" + dateEnd;
+		var all = getAjaxPostData(url).data;
+		var week = getAjaxPostData(getWeekUrl).data.statistics;
+		var weekDate = [];
+		var month = getAjaxPostData(getMonthUrl).data.statistics;
+		var monthDate = [];
+		var temp = [];
+		//把对象分离成一个；
+		for (let key in week) {
+			week[key]['tName'] = key;
+			//添加排序判断值
+			week[key]['index'] = parseInt(key.replace(/[^0-9]/ig, ""));
+			// for(let tmp in week[key]){
+			// 	console.log(tmp+'----'+ week[key][tmp]);
+			// }
+			weekDate.push(week[key])
+		}
+		for (let key in month) {
+			month[key]['tName'] = key;
+			month[key]['index'] = parseInt(key.replace(/[^0-9]/ig, ""));
+			// for(let tmp in month[key]){
+			// 	console.log(tmp+'----'+ month[key][tmp]);
+			// }
+			monthDate.push(month[key])
+		}
+		mao(weekDate);
+		mao(monthDate);
+		// weekDate.sort();
+		temp.push(all);
+		temp.push(weekDate);
+		temp.push(monthDate);
+		// 	var  temp={
+		// 		'all':all,
+		// 		'week':week,
+		// 		'month':month
+		// 	};
+
+		/* $.ajax({
 			type: "post",
 			url: httpUrl() + "/kq/importDailyList?dpid=" + deptId + "&timeStart=" + dateStart + "&timeEnd=" + dateEnd,
 			async: false,
@@ -228,25 +269,42 @@ layui.use(['form', 'table', 'laydate'], function() {
 			contentType: "application/json",
 			dataType: 'json',
 			success: function(res) {
-				if(res.code == '0010') {
+				if (res.code == '0010') {
 					temp = res.data;
-				} else if(res.code == '0031') {
+				} else if (res.code == '0031') {
 					layui.notice.info("提示信息：权限不足");
 				}
 			}
-		});
+		}); */
 		return temp;
 	}
+	//冒泡排序
+	function mao(array) {
+		var arr = array;
+		for (var i = 0; i < arr.length; i++) {
+			for (var j = i; j < arr.length; j++) {
+				if (arr[i].index > arr[j].index) {
+					var tmp = arr[i]; //替换
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		return arr;
+	}
 	$(document).on('click', '#exportBtn', function() {
+		// var data=setExcel(getUser().deptId, dateStart, dateEnd)
 		var str = SplicingTable(temp1);
-		console.log(str)
 		exporExcel("考勤记录", str);
 	});
 	//拼接table与获取数据分开
 	function SplicingTable(array) {
-		var len = array.length;
-		var data = array;
+		var len = array[0].length;
+		var data = array[0];
+		var week = array[1];
+		var month = array[2];
 		var tableStr = '<table border="0" cellspacing="" cellpadding="">'
+		tableStr += '<caption align="top"><h3>员工每日打卡明细</h3></caption>'
 		tableStr += '<tr>';
 		tableStr += '<th width="15%">' + '序号' + '</td>';
 		tableStr += '<th width="15%">' + '姓名' + '</td>';
@@ -258,42 +316,107 @@ layui.use(['form', 'table', 'laydate'], function() {
 		tableStr += '<th width="15%">' + '状态' + '</td>';
 		tableStr += '</tr>';
 		var j = 1
-		for(var i = 0; i < len; i++) {
+		for (var i = 0; i < len; i++) {
 			tableStr += '<tr>';
 			tableStr += '<td>' + j + '</td>';
 			j++;
 			tableStr += '<td>' + data[i].peopleName + '</td>';
 			tableStr += '<td>' + data[i].department + '</td>';
 			tableStr += '<td>' + data[i].jurisdiction + '</td>';
-			if(data[i].stuas == 1) {
+			if (data[i].stuas == 1) {
 				tableStr += '<td>' + '上班打卡' + '</td>';
-			} else if(data[i].stuas == 2) {
+			} else if (data[i].stuas == 2) {
 				tableStr += '<td>' + '下班打卡' + '</td>';
 			}
-			tableStr += '<td>' + new Date(+new Date(data[i].chockinTime) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '') + '</td>'
-			tableStr += '<td>' + new Date(+new Date(data[i].time) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '') + '</td>'
-			if(data[i].stuas2 == 0) {
+			tableStr += '<td>' + new Date(+new Date(data[i].chockinTime) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(
+				/\.[\d]{3}Z/, '') + '</td>'
+			tableStr += '<td>' + new Date(+new Date(data[i].time) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(
+				/\.[\d]{3}Z/, '') + '</td>'
+			if (data[i].stuas2 == 0) {
 				tableStr += '<td>' + '正常' + '</td>';
-			} else if(data[i].stuas2 == 1) {
+			} else if (data[i].stuas2 == 1) {
 				tableStr += '<td style="color:yellow">' + '考勤异常' + '</td>';
-			} else if(data[i].stuas2 == 2) {
+			} else if (data[i].stuas2 == 2) {
 				tableStr += '<td style="color:red">' + '迟到' + '</td>';
-			} else if(data[i].stuas2 == 3) {
+			} else if (data[i].stuas2 == 3) {
 				tableStr += '<td style="color:red">' + '早退' + '</td>';
 			}
 			tableStr += '</tr>';
 		}
-		if(len == 0) {
+		if (len == 0) {
 			tableStr += '<tr style="text-align: center">';
 			tableStr += '<td colspan="8">' + '暂无记录' + '</font></td>';
 			tableStr += '</tr>'
 		}
+		//周
 		tableStr += '</table>';
+		tableStr += '<table border="0" cellspacing="" cellpadding="">'
+		tableStr += '<caption align="top"><h3>部门打卡统计-周</h3></caption>';
+		tableStr += '<tr>';
+		tableStr += '<th width="15%">' + '序号' + '</td>';
+		tableStr += '<th width="15%">' + '名称' + '</td>';
+		tableStr += '<th width="15%">' + '部门人数' + '</td>';
+		tableStr += '<th width="15%">' + '上班考勤' + '</td>';
+		tableStr += '<th width="15%">' + '下班考勤' + '</td>';
+		tableStr += '<th width="15%">' + '上班缺勤' + '</td>';
+		tableStr += '<th width="15%">' + '下班缺勤' + '</td>';
+		tableStr += '<th width="15%">' + '考勤异常' + '</td>';
+		tableStr += '</tr>';
+
+		$.each(week, function(index, item) {
+			tableStr += '<tr>';
+			console.log(item)
+			let i = 1
+			tableStr += '<td>' + i + '</td>';
+			tableStr += '<td>' + item['tName'] + '</td>';
+			tableStr += '<td>' + item['peopleCount'] + '</td>';
+			tableStr += '<td>' + item['shangkao'] + '</td>';
+			tableStr += '<td>' + item['xiakao'] + '</td>';
+			tableStr += '<td>' + item['shangLostKao'] + '</td>';
+			tableStr += '<td>' + item['xiaLostKao'] + '</td>';
+			tableStr += '<td>' + item['yichang'] + '</td>';
+			i++;
+			tableStr += '</tr>';
+		});
+
+		tableStr += '</table>';
+		// 月
+		tableStr += '<table border="0" cellspacing="" cellpadding="">'
+		tableStr += '<caption align="top"><h3>部门打卡统计-月</h3></caption>';
+		tableStr += '<tr>';
+		tableStr += '<th width="15%">' + '序号' + '</td>';
+		tableStr += '<th width="15%">' + '名称' + '</td>';
+		tableStr += '<th width="15%">' + '部门人数' + '</td>';
+		tableStr += '<th width="15%">' + '上班考勤' + '</td>';
+		tableStr += '<th width="15%">' + '下班考勤' + '</td>';
+		tableStr += '<th width="15%">' + '上班缺勤' + '</td>';
+		tableStr += '<th width="15%">' + '下班缺勤' + '</td>';
+		tableStr += '<th width="15%">' + '考勤异常' + '</td>';
+		tableStr += '</tr>';
+		$.each(month, function(index, item) {
+			tableStr += '<tr>';
+			let i = 1
+			tableStr += '<td>' + i + '</td>';
+			tableStr += '<td>' + item['tName'] + '</td>';
+			tableStr += '<td>' + item['peopleCount'] + '</td>';
+			tableStr += '<td>' + item['shangkao'] + '</td>';
+			tableStr += '<td>' + item['xiakao'] + '</td>';
+			tableStr += '<td>' + item['shangLostKao'] + '</td>';
+			tableStr += '<td>' + item['xiaLostKao'] + '</td>';
+			tableStr += '<td>' + item['yichang'] + '</td>';
+			i++;
+			tableStr += '</tr>';
+		});
+		tableStr += '</table>';
+
 		return tableStr;
 	}
 
 	function exporExcel(FileName, excel) {
-		var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
+		let sheetName = ["sheet1", "sheet2", 'sheet3'];
+
+		var excelFile =
+			"<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
 		excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">';
 		excelFile += '<meta http-equiv="content-type" content="application/vnd.ms-excel';
 		excelFile += '; charset=UTF-8">';
@@ -304,7 +427,7 @@ layui.use(['form', 'table', 'laydate'], function() {
 		excelFile += "<x:ExcelWorksheets>";
 		excelFile += "<x:ExcelWorksheet>";
 		excelFile += "<x:Name>";
-		excelFile += "{worksheet}";
+		excelFile += "{考勤明细}";
 		excelFile += "</x:Name>";
 		excelFile += "<x:WorksheetOptions>";
 		excelFile += "<x:DisplayGridlines/>";
