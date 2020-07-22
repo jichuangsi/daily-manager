0646fddfe2521e32b640a261632a6e050c4622c4
NaiCha821 <497839526@qq.com>
2020-07-22 17:38:51 +0800

    欧[sql修改]

---
+++ timingservice/src/main/resources/update_20200722.sql
@@ -0,0 +1,5 @@
+﻿UPDATE roleurl SET url='/kq/importStatisticsByMonth' WHERE id='40282b816c2db5d6016c2dbeb16d786413' ;
+UPDATE roleurl SET url='/kq/importStatisticsByWeek' WHERE id='40542b816c2db5d6016c2dbeb16896556' ;
+UPDATE roleurl SET url='/backstaff/deleteStaffInfo/' WHERE id='40542b816c2db5def1541465454754645' ;
+UPDATE roleurl SET url='/backstaff/frozenStaffInfo/' WHERE id='40542b816c2db5def456789641316' ;
+UPDATE roleurl SET url='/backstaff/thawStaffInfo/' WHERE id='40542b816c2db5def945631345613' ;
